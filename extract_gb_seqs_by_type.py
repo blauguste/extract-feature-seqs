0c11caba2994bb3dfed16c40e0b3767373e8ee76
Auguste Dutcher <Aug@Augustes-MacBook-Pro.local>
2016-10-17 14:58:56 -0700

    first iteration

--- extract_gb_seqs_by_type.py
+++ extract_gb_seqs_by_type.py
@@ -9,7 +9,7 @@ for record in SeqIO.parse('IGR_genbank.gb', 'genbank'):
             sequence_of_interest = feature.location.extract(record).seq
             seqs_of_interest.append(SeqRecord(sequence_of_interest,id=feature.qualifiers['locus_tag'][0]))
 
+with open("IGRs_from_genbank.fasta", "w") as igr_output:
+    SeqIO.write(seqs_of_interest, igr_output, "fasta")
 
-
-#with open("IGRs_from_genbank.fasta", "w") as igr_output:
-    #SeqIO.write(seqs_of_interest, igr_output, "fasta")
+    
